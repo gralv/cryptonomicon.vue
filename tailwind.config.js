df2a7277169732168c2b3fe5ee41f6368fddb34c
current <dzariboo@ukr.net>
2022-05-25 18:09:19 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  },
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          75: 'rgb(247, 247, 247)',
+        },
+      }
+    },
+  },
+  variants:{
+    extend:{
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
